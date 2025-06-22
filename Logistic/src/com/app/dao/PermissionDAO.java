e470427dd82eca75a71f52c6bfe54dc0a816f20c
duble <duble@localhost>
2025-06-22 18:03:07 -0300

    Atulização

--- Logistic/src/com/app/dao/PermissionDAO.java
+++ Logistic/src/com/app/dao/PermissionDAO.java
@@ -10,11 +10,32 @@ public class PermissionDAO implements CRUD<PermissionDTO> {
 
     private static PreparedStatement pstmt;
     private static ResultSet rs;
-    private static Connection conn = new ConnectionDB().getDB();;
+    private static Connection conn = new ConnectionDB().getDB();
+
+    ;
 
     @Override
     public void create(PermissionDTO obj) {
 
+        String queryCreate = "INSERT INTO log_permission(permissionName) values(?)";
+
+        try {
+
+            pstmt = conn.prepareStatement(queryCreate);
+            pstmt.setString(1, obj.getPermissionName());
+            int row = pstmt.executeUpdate();
+
+            if (row > 0) {
+                System.out.println("Sucesso ao salvar os dados.");
+            } else {
+                System.out.println("Erro ao tentar ao salvar os dados.");
+            }
+
+        } catch (Exception e) {
+
+            System.out.println("Erro ao tentar salvar dados: " + e.getMessage());
+
+        }
     }
 
     @Override
@@ -22,22 +43,22 @@ public class PermissionDAO implements CRUD<PermissionDTO> {
 
         String querySelect = "SELECT * FROM log_permission";
         List<PermissionDTO> list = new ArrayList<>();
-                
+
         try {
 
             pstmt = conn.prepareStatement(querySelect);
             rs = pstmt.executeQuery();
-            while(rs.next()){
+            while (rs.next()) {
                 PermissionDTO objDTO = new PermissionDTO();
                 objDTO.setPermissionId(rs.getInt("permissionId"));
                 objDTO.setPermissionName(rs.getString("permissionName"));
                 list.add(objDTO);
             }
-            
+
             rs.close();
             pstmt.close();
             conn.close();
-            
+
         } catch (Exception e) {
 
             System.err.println("Erro ao tentar listar os dados: " + e.getMessage());
@@ -45,7 +66,7 @@ public class PermissionDAO implements CRUD<PermissionDTO> {
         }
 
         return list;
-        
+
     }
 
     @Override
---
+++ Logistic/test/PermissionCreateTest.java
@@ -0,0 +1,17 @@
+
+import com.app.dao.PermissionDAO;
+import com.app.dto.PermissionDTO;
+import java.util.Scanner;
+
+public class PermissionCreateTest {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String permissionName;
+        System.out.print("Escreva o nome da permissão: ");
+        permissionName = in.nextLine().trim().toUpperCase();
+        PermissionDTO objDTO = new PermissionDTO();
+        PermissionDAO objDAO = new PermissionDAO();
+        objDTO.setPermissionName(permissionName);
+        objDAO.create(objDTO);
+    }
+}
